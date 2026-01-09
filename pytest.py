0dea38b097b3b3029b6f38cebeb2082264985c5f
a838831723 <96189270+a838831723@users.noreply.github.com>
2026-01-09 16:34:12 +0800

    Update pytest.py

--- pytest.py
+++ pytest.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
-# import pandas as pd
 import sys
 a = sys.version.split()[0]
 print(a)
 print(sys.executable)
 print(pd.__version__)
-# print(f"当前Python版本: {sys.version.split()[0]}")
-# print(f"Python解释器路径: {sys.executable}")
-# print(f"pandas版本{pd.__version__}")
