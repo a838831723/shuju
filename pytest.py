36b9c7a8205dfb74f494b11ea25f1c10305b1f97
a838831723 <96189270+a838831723@users.noreply.github.com>
2026-01-09 16:43:16 +0800

    Update pytest.py

--- pytest.py
+++ pytest.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 import sys
-a = sys.version.split()[0]
-print(a)
+print(sys.version.split()[0])
 print(sys.executable)
+import os
+for key, value in sorted(os.environ.items()):
+    print(key = value)
+print(os.environ)
